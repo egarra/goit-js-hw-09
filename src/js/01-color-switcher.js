ab769b59f9325729bb9c6529127a3ba2657777e2
egarra <garikhabuzov@gmail.com>
2022-10-24 23:35:15 +0300

    first task hw9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const startBtn = document.querySelector('[data-start]'),
+      stopBtn = document.querySelector('[data-stop]');
+let timerColor;
+      
+startBtn.addEventListener('click', () => {
+    document.body.style.backgroundColor = getRandomHexColor();
+    timerColor = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    },1000);
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+})
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(timerColor);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+})
 
