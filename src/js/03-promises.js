1f8300786127e14c6ab17b1599b44afa3c4de58d
egarra <garikhabuzov@gmail.com>
2022-10-25 21:59:47 +0300

    task 3 complete

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,23 +2,46 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const inputForm = document.querySelector('.form');
 
-const { elements: {
+let { elements: {
   delay,
   step,
   amount
 }} = inputForm;
 
-console.log(delay);
+function createPromise({i, delay}) {
 
-function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    resolve(Notify.success('Sol lucet omnibus'));
-    // Fulfill
-  } else {
-    reject(Notify.failure('Please choose a date in the future'));
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+    
+      if (shouldResolve) {
+        resolve({i, delay})
+      } else {
+        reject({i, delay});
+      }
+    }, delay)
+  })
+  return promise;
 }
+
+inputForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  delay = Number(delay.value);
+  step = Number(step.value);
+
+  for (let i = 0; i < Number(amount.value); i++) {
+
+    createPromise({i, delay})
+        .then(({i, delay}) => {
+          console.log(delay)
+          Notify.success(`Fulfilled promise ${i + 1} in ${delay}ms`)
+        })
+        .catch(({i, delay}) => Notify.failure(`Rejected promise ${i + 1} in ${delay}ms`))
+
+        delay += step;
+    console.log(delay)
+  }
+})
 
 
